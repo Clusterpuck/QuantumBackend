2b316cd89b07534432cddc4e7de3fb5339b79b1b
Song Yi Loo <songyi.loo@gmail.com>
2024-08-06 01:26:13 +0800

    Trimmed down quantum code for tight integration

---
+++ route_optimisation/route_solver/dwave_solver.py
@@ -0,0 +1,204 @@
+from dwave.system.composites import EmbeddingComposite
+from dwave.system.samplers import DWaveSampler
+import numpy as np
+import dimod
+
+import os
+
+from route_solver import RouteSolver
+from quantum_tsp.tsp_formulation import TSPFormulation
+
+
+class DWaveSolver(RouteSolver):
+
+    def __init__(
+        self,
+        cost_factor: int = 10,
+        constraint_factor: int = 800,
+        chain_strength: int = 800,  # TODO: Check if chain breaks is a problem based on valid solution count and chain_break_frequency. If so, bump it to 1000
+        num_runs: int = 1024,  # Might want to override this depending on problem size and confidence...
+        max_retries: int = 3,
+        is_circuit: bool = False,
+    ):  # Since the D-Wave strategy is fiddly, initialise with extra details
+        """
+        Inits an ATSP solver that uses D-Wave machines.
+
+        TODO: Plan to allow a few more post-init adjustable hyperparams
+        TODO: Plan to move qubo init into public function that can be called again post init with optional new data, allowing either hyperparam optimisation for the same problem or new problem with config reuse
+
+        Parameters
+        ----------
+        cost_factor : int, default=10
+            Scales QUBO weight for costs. Cannot exceed constraint factor.
+        constraint_factor : int, default=800
+            Scales QUBO weight for penalising constraint violations.
+        chain_strength : int, default=800
+            Decreases likelihood of chain breaks, but drifts from original problem definition.
+        num_runs : int, default=1024
+            More runs increase probability of finding the best solution, but may hit cap or runtime limit.
+        max_retries : int, default=3
+            Max reattempts if a valid route could not be found.
+        is_circuit : bool, default=False
+            If False, reformulates as a minimum path variation.
+        """
+        self.__cost_factor = cost_factor
+        self.__constraint_factor = constraint_factor
+        self.__chain_strength = chain_strength
+        self.__num_runs = num_runs
+        self.__max_retries = max_retries
+        self.__is_circuit = is_circuit
+
+        # Create sampler
+        sampler_config = {
+            "token": os.environ[
+                "DWAVE_TOKEN"
+            ],  # TODO: Might need to validate these somewhere else
+            "endpoint": os.environ["DWAVE_URL"],
+            "solver": os.environ["DWAVE_SOLVER"],
+        }
+        self.__sampler = EmbeddingComposite(DWaveSampler(**sampler_config))
+
+    def solve(self, distance_matrix: np.ndarray) -> tuple[list[int], int]:
+        """
+        Compute a good solution for TSP via D-Wave.
+
+        Parameters
+        ----------
+        distance_matrix : ndarray
+            Asymmetric distance matrix to find a shortest route for.
+
+        Returns
+        -------
+        best_route : list of int
+            The valid route with the lowest energy.
+        int
+            Distance if the route is followed.
+
+        Raises
+        ------
+        ValueError
+            If distance matrix is invalid.
+        RuntimeError
+            If D-Wave cannot find a valid solution within the number of retries
+        """
+        # Fast fail
+        if len(distance_matrix) <= 2:
+            raise ValueError("Distance matrix must be 2x2 or larger.")
+        elif distance_matrix.shape[0] != distance_matrix.shape[1]:
+            raise ValueError("Distance matrix must be square.")
+        elif len(distance_matrix) > 10:
+            raise ValueError("D-Wave cannot handle more than 10 TSP nodes.")
+
+        # Create QUBO
+        qubo_formulator = TSPFormulation()
+        qubo = qubo_formulator.formulate(
+            distance_matrix,
+            self.__cost_factor,
+            self.__constraint_factor,
+            self.__is_circuit,
+        )
+
+        # Try sending to D-Wave machine
+        retries = 0
+        best_route = None
+        while best_route is not None and retries < self.__max_retries:
+            response = self.__sampler.sample_qubo(
+                qubo, chain_strength=self.__chain_strength, num_reads=self.__num_runs
+            )
+            best_route = self.__decode_solution(response)
+
+        if retries < self.__max_retries:
+            raise RuntimeError("No valid D-Wave solution received")
+            # TODO: Implement a custom error that doesn't hard crash, or else return a guess?
+
+        current_cost = self.__get_route_cost(
+            best_route, distance_matrix, self.__is_circuit
+        )
+
+        return best_route, current_cost
+
+    def __get_route_cost(
+        self, route: list[int], distance_matrix: np.ndarray, is_circuit: bool
+    ) -> int:
+        """
+        Calculate the cost of a route solution.
+
+        Parameters
+        ----------
+        route : list of int or None
+            Complete, indexed locations in visit order.
+        distance_matrix : ndarray
+            Data to compute the total distance with.
+        is_circuit : bool
+            Whether to include the loop back to start.
+
+        Returns
+        -------
+        int
+            Distance if the route is followed.
+        """
+        cost = 0
+        for i in range(len(route) - 1):
+            cost += distance_matrix[route[i], route[i + 1]]
+
+        # Loop around if circuit
+        if is_circuit:
+            cost += distance_matrix[route[-1], route[0]]
+
+        return cost
+
+    def __validate_permutation(self, matrix: np.ndarray) -> bool:
+        """
+        Validate that permutation matrix is correctly one-hot encoded.
+
+        Parameters
+        ----------
+        matrix : ndarray
+            Permutation matrix. Assumes binary matrix.
+
+        Returns
+        -------
+        bool
+            True if one-hot encoded.
+        """
+        # Every row/col sums to 1
+        return np.all(np.sum(matrix, axis=0) == 1) and np.all(
+            np.sum(matrix, axis=1) == 1
+        )
+
+    def __decode_solution(self, response: dimod.SampleSet) -> list[int] | None:
+        """
+        Decodes BV permutation matrix, returning 0-based route.
+
+        Parameters
+        ----------
+        response : SampleSet
+            Response from D-Wave (QUBO-specific). Contains sampled info in a
+            NumPy recarray format. See official docs: https://docs.ocean.dwavesys.com/en/stable/docs_dimod/reference/sampleset.html
+
+        Returns
+        -------
+        best_solution : list of int or None
+            The valid route with the lowest energy, if found.
+        """
+        valid_distribution = {}
+        min_energy = np.argmax(response.record.energy)
+        best_solution = None
+        node_count = int(np.sqrt(len(response.record[0].sample)))
+
+        for entry in response.record:
+            solution_matrix = np.reshape(entry.sample, (node_count, node_count))
+
+            # Filter to only valid ones
+            if self.__validate_permutation(solution_matrix):
+                solution = [np.where(row == 1)[0][0] for row in solution_matrix]
+                valid_distribution[tuple(solution)] = (
+                    entry.energy,
+                    entry.num_occurrences,
+                )
+                # Track the best one
+                if entry.energy <= min_energy:
+                    min_energy = entry.energy
+                    best_solution = solution
+
+        return best_solution
---
+++ route_optimisation/route_solver/quantum_tsp/tsp_formulation.py
@@ -0,0 +1,181 @@
+import numpy as np
+
+
+class TSPFormulation:
+    """
+    Class for constructing a Travelling Salesman Problem QUBO.
+
+    Based on Andrew Lucas' Ising formulation and BOHRTECHNOLOGY's QUBO
+    implementation. Supports asymmetric costs and paths, but not fixed
+    start point (yet).
+    """
+
+    # Helper functions
+    def __add_cost_objective(
+        self, cost_factor: int, distance_matrix: np.ndarray, circuit: bool
+    ) -> dict[tuple[int, int], int]:
+        """
+        Encodes asymmetric distances as QUBO edges between sequential nodes.
+
+        Runs in O(n^3), creating n^2(n-1) new edges. If path, n(n-1)^2.
+
+        Parameters
+        ----------
+        cost_factor : int
+            Scaling factor to adjust distance weighting.
+        distance_matrix : ndarray
+            Asymmetric distance matrix to find a shortest route for.
+        circuit : bool
+            If True, disconnects last node from first to force path discovery.
+
+        Returns
+        -------
+        dict
+            Collection of QUBO edge weights, indexed by 2-tuple of BV indices.
+            Note that QUBO just sums (x, y) and (y, x) edges.
+        """
+        qubo_dict = {}
+        n = len(distance_matrix)
+
+        # If path, do not connect end back to start. Neither reward nor
+        # penalise, acting like a dummy 0 cost node without the node wastage.
+        max_row = n if circuit else n - 1
+
+        # Links each BV to the row below (represents travel cost to next node)
+        for row in range(max_row):
+            for col in range(n):
+                qubit_a = row * n + col
+                for col_iter in range(n):
+                    if col != col_iter:  # Avoid one-hot violations
+                        qubit_b = (row + 1) % n * n + col_iter
+                        qubo_dict[(qubit_a, qubit_b)] = (
+                            cost_factor * distance_matrix[col][col_iter]
+                        )
+
+        return qubo_dict
+
+    def __add_selection_incentive(
+        self, constraint_factor: int, n: int
+    ) -> dict[tuple[int, int], int]:
+        """
+        Encourages variable selection, effectively discouraging less than n
+        selections.
+
+        Runs in O(n^2), creating n^2 new edges (may as well be self-weights).
+
+        Parameters
+        ----------
+        constraint_factor : int
+            Scaling factor to adjust constraint weighting.
+        n : int
+            Number of nodes in a route.
+
+        Returns
+        -------
+        dict
+            Collection of QUBO edge weights, indexed by 2-tuple of BV indices.
+            Note that QUBO just sums (x, y) and (y, x) edges.
+        """
+        qubo_dict = {}
+
+        # Basically, reward every individual variable selection
+        for row in range(n):
+            for col in range(n):
+                qubit_a = row * n + col
+                qubo_dict[(qubit_a, qubit_a)] = -constraint_factor
+
+        return qubo_dict
+
+    def __add_time_constraints(
+        self, constraint_factor: int, n: int
+    ) -> dict[tuple[int, int], int]:
+        """
+        Strongly penalises being in multiple places at the same time (aka
+        horizontal one-hot violations). When paired with position constraints,
+        this inherently discourages more than n selections.
+
+        Runs in O(n^3), creating n(n-1)^2 new edges.
+
+        Parameters
+        ----------
+        constraint_factor : int
+            Scaling factor to adjust constraint weighting.
+        n : int
+            Number of nodes in a route.
+
+        Returns
+        -------
+        dict
+            Collection of QUBO edge weights, indexed by 2-tuple of BV indices.
+            Note that QUBO just sums (x, y) and (y, x) edges.
+        """
+        qubo_dict = {}
+
+        for row in range(n):
+            for col in range(n - 1):  # Link cols to their right-ward BVs
+                qubit_a = row * n + col
+                for col_offset in range(col + 1, n):
+                    qubit_b = row * n + col_offset
+                    qubo_dict[(qubit_a, qubit_b)] = 2 * constraint_factor
+                    # Penalty must be strong enough to offset variable selection incentive, hence double
+                    # TODO: Check if double is needed, since any exceedance inherently violates multiple times anyways
+
+        return qubo_dict
+
+    def __add_position_constraints(
+        self, constraint_factor: int, n: int
+    ) -> dict[tuple[int, int], int]:
+        """
+        Strongly penalises revisiting the same node (aka vertical one-hot
+        violations). When paired with time constraints, this inherently
+        discourages more than n selections.
+
+        Runs in O(n^3), creating n(n-1)^2 new edges.
+
+        Parameters
+        ----------
+        constraint_factor : int
+            Scaling factor to adjust constraint weighting.
+        n : int
+            Number of nodes in a route.
+
+        Returns
+        -------
+        dict
+            Collection of QUBO edge weights, indexed by 2-tuple of BV indices.
+            Note that QUBO just sums (x, y) and (y, x) edges.
+        """
+        qubo_dict = {}
+
+        # Same as time penalty, but downwards instead
+        for row in range(n - 1):
+            for col in range(n):
+                qubit_a = row * n + col
+                for row_offset in range(row + 1, n):
+                    qubit_b = row_offset * n + col
+                    qubo_dict[(qubit_a, qubit_b)] = 2 * constraint_factor
+
+        return qubo_dict
+
+    # Public methods
+    def formulate(
+        self,
+        distance_matrix: np.ndarray,
+        cost_factor: int,
+        constraint_factor: int,
+        is_circuit: bool,
+    ) -> dict[tuple[int, int], int]:
+        # Normalise, so that relative scaling factors work
+        max_distance = np.max(np.array(distance_matrix))
+        scaled_matrix = distance_matrix / max_distance
+        n = len(distance_matrix)
+
+        # Enforce minimum length
+        cost_terms = self.__add_cost_objective(cost_factor, scaled_matrix, is_circuit)
+
+        # Enforce Hamiltonian path
+        incentive_terms = self.__add_selection_incentive(constraint_factor, n)
+        time_times = self.__add_time_constraints(constraint_factor, n)
+        position_terms = self.__add_position_constraints(constraint_factor, n)
+
+        return cost_terms | incentive_terms | time_times | position_terms
