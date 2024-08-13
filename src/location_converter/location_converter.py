f0d6261455ba838d602bbbf85c24fecddf34c293
Scrubzie <Scrubzzilla@gmail.com>
2024-08-13 21:10:55 +0800

    Reduced to a single function

--- src/location_converter/location_converter.py
+++ src/location_converter/location_converter.py
@@ -1,43 +1,38 @@
+"""Transform latitude and longitude using equirectangular projection"""
 import numpy as np
-from pydantic_models import OrderInput
 
-def locations_to_2D(locations: list[OrderInput]):
-    # This must convert geographic to 2D
+def geographic_to_2d(latitudes: list, longitudes: list) -> tuple[np.array, np.array]:
+    """
+    Use an approximation of equirectangular projection to map latitude and longitude
+    to a 2D plane.
 
-    latitudes = [location.lat for location in locations]
-    longitudes = [location.long for location in locations]
+    Parameters
+    ----------
+    latitudes : list of floats
+        List of latitudes for orders
+    longitudes : list of floats
+        List of latitudes for orders
 
+    Returns
+    -------
+    latitudes : list of floats
+        List of latitudes for orders 
+    longitudes : list of floats
+        List of latitudes for orders
+    """
     latitudes = np.array(latitudes)
     longitudes = np.array(longitudes)
 
-    R = 6371  # Radius of Earth
+    r = 6371  # Radius of Earth
     centre_point_deg = -31.952258602714696
 
     # Convert to radians
     center_latitude_radians = np.radians(centre_point_deg)
     longitudes_radians = np.radians(longitudes)
     latitudes_radians = np.radians(latitudes)
-    
+
     # Apply equirectangular projection
-    longitudes = R * longitudes_radians * np.cos(center_latitude_radians)
-    latitudes = R * latitudes_radians
-    
-    return latitudes, longitudes
+    x = r * longitudes_radians * np.cos(center_latitude_radians)
+    y = r * latitudes_radians
 
-def geographic_to_2D(latitudes, longitudes):
-    latitudes = np.array(latitudes)
-    longitudes = np.array(longitudes)
-
-    R = 6371  # Radius of Earth
-    centre_point_deg = -31.952258602714696
-
-    # Convert to radians
-    center_latitude_radians = np.radians(centre_point_deg)
-    longitudes_radians = np.radians(longitudes)
-    latitudes_radians = np.radians(latitudes)
-    
-    # Apply equirectangular projection
-    longitudes = R * longitudes_radians * np.cos(center_latitude_radians)
-    latitudes = R * latitudes_radians
-    
-    return latitudes, longitudes
+    return y, x
