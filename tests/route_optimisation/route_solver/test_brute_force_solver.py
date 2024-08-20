fc88bb52287001c4fdd7a76a5c665a6c0cfc397c
Scrubzie <Scrubzzilla@gmail.com>
2024-08-20 21:53:30 +0800

    Added File

---
+++ tests/route_optimisation/route_solver/test_brute_force_solver.py
@@ -0,0 +1,12 @@
+import pytest
+from route_optimisation.route_solver.brute_force_solver import BruteForceSolver
+
+# Solve function will always take in an np distance matrix
+# Wrong Types
+# Weird Matrix Shapes
+# Weird Values
+# Standard Tests
+# No Route
+# Equal Costs
+# Single Node
+# 2x2
