5b7de2def965a2447b7c782860e1d1480e26d29a
Scrubzie <Scrubzzilla@gmail.com>
2024-08-26 20:16:08 +0800

    Updated docstrings, reformatted

--- src/distance_factory.py
+++ src/distance_factory.py
@@ -1,3 +1,5 @@
+"""Factory for distance metric"""
+
 import os
 
 from route_optimisation.distance_matrix.cartesian_distance_finder import (
@@ -11,8 +13,29 @@ from route_optimisation.distance_matrix.distance_finder import DistanceFinder
 
 
 class DistanceFactory:
-    # Validate and decide what distance metric the solver will use for the API
+    """
+    Factory class for creating a particular DistanceFinder instance
+    """
+
     def create(self, distance_type: str) -> DistanceFinder:
+        """
+        Validate and decide what distance metric the solver will use for the API
+
+        Parameters
+        ----------
+        distance_type : str
+            distace metric to be used
+
+        Returns
+        -------
+        DistanceFinder
+            DistanceFinder strategy depending on distance_type
+
+        Raises
+        ------
+        ValueError
+            If provided distance_type doesn't exist.
+        """
         if distance_type == "cartesian":
             return CartesianDistanceFinder()
         elif distance_type == "mapbox":
