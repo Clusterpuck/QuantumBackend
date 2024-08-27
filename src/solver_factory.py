3e7316e84f8ec5befc8d91a5e066fe97522fce91
Scrubzie <Scrubzzilla@gmail.com>
2024-08-27 20:04:57 +0800

    Updated docstrings, refactored, reformatted

--- src/solver_factory.py
+++ src/solver_factory.py
@@ -1,6 +1,9 @@
+"""Factory for solving method"""
+
+import os
+
 from dwave.system.samplers import DWaveSampler
 from dwave.system.composites import EmbeddingComposite
-import os
 
 from route_optimisation.route_solver.brute_force_solver import BruteForceSolver
 from route_optimisation.route_solver.dwave_solver import DWaveSolver
@@ -8,8 +11,30 @@ from route_optimisation.route_solver.route_solver import RouteSolver
 
 
 class SolverFactory:
+    """
+    Factory class for creating a particular SolverFactory instance
+    """
+
     # Validate and decide what ATSP solver to build for the API
     def create(self, solver_type: str) -> RouteSolver:
+        """
+        Validate and decide what solving method to use for the API
+
+        Parameters
+        ----------
+        solver_type : str
+            solving method to be used
+
+        Returns
+        -------
+        RouteSolver
+            RouteSolver strategy depending on solver_type
+
+        Raises
+        ------
+        ValueError
+            If provided solver_type doesn't exist.
+        """
         if solver_type == "dwave":
             # Max cluster size is already handled by the dwave solver
             # TODO: Might need to handle bad k on the endpoint to prevent 500s?
@@ -26,8 +51,7 @@ class SolverFactory:
             )
             # Missing tokens are the app's (env) fault, so bubble up
 
-        elif solver_type == "brute":
+        if solver_type == "brute":
             return BruteForceSolver()
 
-        else:
-            raise ValueError("Unsupported solver type.")
+        raise ValueError("Unsupported solver type.")
