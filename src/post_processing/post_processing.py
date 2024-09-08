4ed0d446fa87817f33ba40f1d894d9e0811e1b4d
Scrubzie <Scrubzzilla@gmail.com>
2024-09-08 18:24:46 +0800

    Added docstrings, reformatted

--- src/post_processing/post_processing.py
+++ src/post_processing/post_processing.py
@@ -2,15 +2,31 @@
 
 import sys
 import os
-import pandas as pd
 import ast
+import pandas as pd
 import graph_storer as gs
 import route_storer as rs
 
 # cd to post_processing
 # python post_processing.py "output" "test1" "Locations.json"
 
+
 def post_process() -> None:
+    """
+    Main method for post-processing of parameter sweeping.
+    Reads a CSV and makes relevant graphs for analysis.
+
+    Uses command line arguments
+
+    Parameters
+    ----------
+    input_name: str
+        File name for input CSV
+    output_file: str
+        File name for output files
+    orders_file: str
+        File name containing payload of orders
+    """
     input_name = sys.argv[1]
     output_file = sys.argv[2]
     orders_file = sys.argv[3]
@@ -23,8 +39,19 @@ def post_process() -> None:
     failed_occurences(df, output_file)
     visualise_deliveries(df, orders_file)
 
-def average(success_df, output_file) -> None:
+
+def average(success_df: pd.DataFrame, output_file: str) -> None:
     # Average performance
+    """
+    Save a heatmap and contour plot of average performance
+
+    Parameters
+    ----------
+    success_df: DataFrame
+        Contains successful trials
+    output_file: str
+        File name for output files
+    """
     average_df = (
         success_df.groupby(["cost_constraint_ratio", "chain_strength"])
         .agg({"relative_cost": "mean"})
@@ -45,7 +72,18 @@ def average(success_df, output_file) -> None:
         ("avg", output_file),
     )
 
-def best(success_df, output_file) -> None:
+
+def best(success_df: pd.DataFrame, output_file: str) -> None:
+    """
+    Save a heatmap and contour plot of best route
+
+    Parameters
+    ----------
+    success_df: DataFrame
+        Contains successful trials
+    output_file: str
+        File name for output files
+    """
     idx = success_df.groupby(["cost_constraint_ratio", "chain_strength"])[
         "relative_cost"
     ].idxmin()
@@ -66,7 +104,18 @@ def best(success_df, output_file) -> None:
         ("best", output_file),
     )
 
-def failed_occurences(df, output_file) -> None:
+
+def failed_occurences(df: pd.DataFrame, output_file: str) -> None:
+    """
+    Save a heatmap of failed number of occurences
+
+    Parameters
+    ----------
+    df: DataFrame
+        Contains raw data from CSV
+    output_file: str
+        File name for output files
+    """
     failed_routes_df = df[df["relative_cost"] == -1]
     if not failed_routes_df.empty:
         failed_routes_df = (
@@ -84,7 +133,19 @@ def failed_occurences(df, output_file) -> None:
             ("fails", output_file),
         )
 
-def visualise_deliveries(df, orders_file) -> None:
+
+def visualise_deliveries(df: pd.DataFrame, orders_file: str) -> None:
+    """
+    For every found route, graph the route associated.
+    Outputs files to data/routes
+
+    Parameters
+    ----------
+    df: DataFrame
+        Contains raw data from CSV
+    orders_file: str
+        JSON file name containing payload
+    """
     for _, row in df.iterrows():
         cost_constraint_ratio = str(row["cost_constraint_ratio"])
         chain_strength = str(row["chain_strength"])
@@ -96,5 +157,6 @@ def visualise_deliveries(df, orders_file) -> None:
         route_list = ast.literal_eval(row["route"])
         rs.create_graph(orders_file, route_list, "routes", filename)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     post_process()
