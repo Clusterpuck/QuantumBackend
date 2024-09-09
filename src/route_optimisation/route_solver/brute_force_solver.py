023df5cf3ea7e756a85ebd128e73109b38177f86
Scrubzie <Scrubzzilla@gmail.com>
2024-09-09 20:12:55 +0800

    Fixed docstrings

--- src/route_optimisation/route_solver/brute_force_solver.py
+++ src/route_optimisation/route_solver/brute_force_solver.py
@@ -4,28 +4,32 @@ from itertools import permutations
 import numpy as np
 from route_optimisation.route_solver.route_solver import RouteSolver
 
+
 class BruteForceSolver(RouteSolver):
     """
-    Generates every permutation of routes and finds the optimal route
-
-    Parameters
-    ----------
-    distance_matrix: ndarray
-        2D, Asymmetric distance matrix
-
-    Returns
-    -------
-    tuple[list[int], int]
-        List[int] represents the list of the optimal route containing order_ids
-        int represents the total cost of the route
+    Find the optimal route in a distance matrix
     """
-    def solve(self, distance_matrix: np.ndarray) -> tuple[list[int], int]:
 
+    def solve(self, distance_matrix: np.ndarray) -> tuple[list[int], int]:
+        """
+        Generates every permutation of routes and finds the optimal route
+
+        Parameters
+        ----------
+        distance_matrix: ndarray
+            2D, Asymmetric distance matrix
+
+        Returns
+        -------
+        tuple[list[int], int]
+            List[int] represents the list of the optimal route containing order_ids
+            int represents the total cost of the route
+        """
         n = len(distance_matrix)
         index = list(range(n))
         all_routes = permutations(index)
 
-        lowest_cost = float('inf')
+        lowest_cost = float("inf")
         best_route = []
         for route in all_routes:
             current_cost = self.__get_route_cost(route, distance_matrix)
@@ -34,12 +38,30 @@ class BruteForceSolver(RouteSolver):
                 best_route = route
         return list(best_route), lowest_cost
 
-    def __get_route_cost(self, route, distance_matrix):
+    def __get_route_cost(
+        self, route: tuple[int, ...], distance_matrix: np.ndarray
+    ) -> float:
+        """
+        Calculates the cost of a route
+
+        Parameters
+        ----------
+        route: tuple[int, ...]
+            A tuple of order ids
+            The size should equal be as long as the matrix
+        distance_matrix: ndarray
+            2D, Asymmetric distance matrix
+
+        Returns
+        -------
+        cost: float
+            The cost of a route
+        """
         cost = 0
         # Range from 0 to size of route (no need to add cost of last node)
         order = range(len(route) - 1)
 
         for i in order:
             # Add cost of current node and path to the next
-            cost += distance_matrix[route[i]][route[i + 1]] 
+            cost += distance_matrix[route[i]][route[i + 1]]
         return cost
